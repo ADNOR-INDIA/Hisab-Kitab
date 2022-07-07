ea0a3a83f6a446125819f7c9f05e832f24fdccb5
Abhay Gupta <adnorindia@gmail.com>
2022-07-07 16:51:00 +0530

    new components added

---
+++ Client/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from './Footer.module.css'
+
+
+const Footer = ()=>{
+    return(
+        <footer>
+            <div className={styles.footerText}>
+            ©Abhay Gupta || Special Thanks to Panshak Solomon for project idea.
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
